1936dcfd1a78812bdf0a42aa31cb8bd1dc582953
Danilo Silva <danilo.silva@msn.com>
2023-01-30 20:15:24 -0300

    Implementação BaseEntity

---
+++ Ecommerce/Ecommerce.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            this.DataCriacao = DateTime.Now;
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataModificacao { get; set; }
+    }
+}
