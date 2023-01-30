f151249e9756880ea3c712ca6a61d2fc6e6ab330
Danilo Silva <danilo.silva@msn.com>
2023-01-30 20:15:37 -0300

    Inclusão da entidade Cliente

---
+++ Ecommerce/Ecommerce.Domain/Entities/Cliente.cs
@@ -0,0 +1,20 @@
+namespace Ecommerce.Domain.Entities
+{
+    public class Cliente : BaseEntity
+    {
+        public Cliente(string nome, string sobrenome, string email, string numeroContato, string endereco)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Email = email;
+            NumeroContato = numeroContato;
+            Endereco = endereco;
+        }
+
+        public string Nome { get; private set; }
+        public string Sobrenome { get; private set; }
+        public string Email { get; private set; }
+        public string NumeroContato { get; private set; }
+        public string Endereco { get; private set; }
+    }
+}
